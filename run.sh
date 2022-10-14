0d3445ef437e30d3838f829114ccf2194a2e6246
Jörg Wesemann (jweraspi64) <joerg@weseit.de>
2022-10-14 19:53:18 +0200

    removed port exposure 8000 for default start in run.sh

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ c=${a##*-}
 echo $c
 
 
-docker run -d -p 8000:8000 -p 9443:9443 --name ${c} --restart=always -v /var/run/docker.sock:/var/run/docker.sock  -v ${c}:/data  ${c}/portainer-ce:latest
+#docker run -d -p 8000:8000 -p 9443:9443 --name ${c} --restart=always -v /var/run/docker.sock:/var/run/docker.sock  -v ${c}:/data  ${c}/portainer-ce:latest
+docker run -d -p 9443:9443 --name ${c} --restart=always -v /var/run/docker.sock:/var/run/docker.sock  -v ${c}:/data  ${c}/portainer-ce:latest
 docker ps
 
